9f0a8020372503676c2e42678513ba77f62fe5f3
Josiel <lucasjosiel3@gmail.com>
2026-03-17 23:10:11 -0300

    corrigindo fetch no bot

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
+const fetch = require('node-fetch');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
